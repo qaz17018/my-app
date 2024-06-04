2608e0bc193fa701c9cb15e6608fee3fdfe97022
= <=>
2024-06-04 18:06:31 +0800

    css test

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -2,8 +2,9 @@ import React from 'react';
 import '../styles/Explore.css';
 
 const Explore = () => (
-    <div className="explore">
-        <h1>Explore</h1>
+    <div className='xixi'>
+        <h1 className='hahaha'>Explore</h1>
+        <h2 >dasd</h2>
     </div>
 );
 
